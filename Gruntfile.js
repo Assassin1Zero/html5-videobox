356e9bf423cf7377ee6d1581a00caf8c8217a4d1
Stuart Williams <Stuart_Williams@discovery.com>
2016-05-11 16:32:54 +0100

    created dni & eurosport build tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,19 +20,86 @@ module.exports = function(grunt) {
                 src: ['build']
             },
             post: {
-                src: ['build/main.js']
+                src: ['build/main.js', 'build/main.min.js']
             }
         },
         copy: {
-        	build: {
-        		files: [
-        			{src: 'template.html', dest: 'build/index.html'},
-                    {expand: true, src: 'css/**', dest: 'build/', flatten: true, filter: 'isFile'},
-                    {expand: true, src: 'js/**', dest: 'build/', flatten: true, filter: 'isFile'},
-                    {expand: true, src: 'images/**', dest: 'build/', flatten: true, filter: 'isFile'},
-                    {expand: true, src: 'media/**', dest: 'build/', flatten: true, filter: 'isFile'},
-        		]
-        	}
+            build_eursport: {
+                files: [{
+                    src: 'template.html',
+                    dest: 'build/eurosport-videobox/index.html'
+                }, {
+                    expand: true,
+                    src: 'css/**',
+                    dest: 'build/eurosport-videobox/',
+                    flatten: true,
+                    filter: 'isFile'
+                }, {
+                    expand: true,
+                    src: 'js/**',
+                    dest: 'build/eurosport-videobox/',
+                    flatten: true,
+                    filter: 'isFile'
+                }, {
+                    src: 'build/main.js',
+                    dest: 'build/eurosport-videobox/main.min.js'
+                }, {
+                    expand: true,
+                    src: 'images/**',
+                    dest: 'build/eurosport-videobox/',
+                    flatten: true,
+                    filter: 'isFile'
+                }, {
+                    expand: true,
+                    src: 'media/**',
+                    dest: 'build/eurosport-videobox/',
+                    flatten: true,
+                    filter: 'isFile'
+                }],
+                options: {
+                    noProcess: [
+                        '**/*.{png,mp4,jpg}'
+                    ],
+                    process: function(content, source) {
+
+                        if (/template.html/.test(source)) {
+                            content = content.replace('autoplay: true', 'autoplay: true,\n        clickUrl: \'[CLICK MACRO HERE]\'');
+                            //content = content.replace(/<(\/?|\!?)(DOCTYPE html|html.*|head|body|meta.*|title.*)>/gm, '');
+                            content = content.replace(/<(\/?|\!?)(DOCTYPE html|html.*|head|body|meta.*|title.*)>(\n|\t)/gm, '');
+                            content = content.replace('css/styles.css', 'styles.css');
+                            content = content.replace('js/main.min.js', 'main.min.js');
+                            content = content.replace(/media\//gm, '');
+                        }
+
+                        if (/styles.css/.test(source)) {
+                            content = content.replace('background-image: url(../images/spritesheet.png)',
+                                'background-image: url(spritesheet.png)');
+                        }
+                        return content;
+                    }
+                }
+            },
+            build_dni: {
+                files: [{
+                    src: 'template.html',
+                    dest: 'build/dni-videobox/index.html'
+                }, {
+                    src: 'css/**',
+                    dest: 'build/dni-videobox/'
+                }, {
+                    src: 'js/**',
+                    dest: 'build/dni-videobox/'
+                }, {
+                    src: 'build/main.min.js',
+                    dest: 'build/dni-videobox/js/main.min.js'
+                }, {
+                    src: 'images/**',
+                    dest: 'build/dni-videobox/'
+                }, {
+                    src: 'media/**',
+                    dest: 'build/dni-videobox/'
+                }, ]
+            }
         },
         uglify: {
             build: {
@@ -51,8 +118,8 @@ module.exports = function(grunt) {
             }
         },
         watch: {
-        	files: ['lib/**', 'css/**', 'images/**', 'js/**', 'template.html'],
-        	tasks: ['clean:pre', 'browserify', 'uglify','copy', 'clean:post']
+            files: ['lib/**', 'css/**', 'images/**', 'js/**', 'template.html'],
+            tasks: ['clean:pre', 'browserify', 'uglify', 'copy', 'clean:post']
         }
     });
 
@@ -61,7 +128,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     // Default task.
-    grunt.registerTask('default', ['clean:pre', 'browserify', 'uglify','copy', 'clean:post']);
+    grunt.registerTask('default', ['clean:pre', 'browserify', 'uglify', 'copy', 'clean:post']);
 };
